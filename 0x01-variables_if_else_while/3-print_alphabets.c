c29154a43a91793d54ef078dc5be4bb1eea202bd
piustech <barimue.emmanuel@gmail.com>
2022-12-09 09:32:16 -0800

    3. alphABET

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -4,17 +4,17 @@
 
 /**
  * main - Entry point
- *    
+ *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
 	char ch;
-	
-	for (ch = 'a'; ch <= 'z'; ch++)   
-		putchar(ch);   
-	for (ch = 'A'; ch <= 'Z'; ch++)  
-		putchar(ch);  
-	putchar('\n'); 
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+		putchar(ch);
+	for (ch = 'A'; ch <= 'Z'; ch++)
+		putchar(ch);
+	putchar('\n');
 	return (0);
 }
