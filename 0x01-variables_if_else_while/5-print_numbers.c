31efae7e7e849d6a636c296619e0a3d5e488b332
piustech <barimue.emmanuel@gmail.com>
2022-12-09 10:20:24 -0800

    5. Numbers

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,6 +1,4 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
 
 /**
  * main - Entry point
@@ -14,7 +12,6 @@ int main(void)
 	for (i = 0; i < 10; i++
 		printf("%d", i);
 	printf("\n");
-
 	return (0);
 }
 			}
