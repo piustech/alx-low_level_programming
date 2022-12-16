3509e0ed10f57a2e74c2648ada93758db0c1e8be
piustech <barimue.emmanuel@gmail.com>
2022-12-16 04:54:34 -0800

    Task 3. Leap year

--- 0x03-debugging/3-main_b.c
+++ 0x03-debugging/3-main_b.c
@@ -6,15 +6,16 @@
  * leap years into account
  * Return: 0
  */
+
 int main(void)
 {
 	int month;
 	int day;
 	int year;
 
-	month = 4;
-	day = 01;
-	year = 1997;
+	month = 2;
+	day = 29;
+	year = 2000;
 
 	printf("Date: %02d/%02d/%04d\n", month, day, year);
 
