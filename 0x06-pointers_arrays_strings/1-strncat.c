4aed62ec9b55636d5333fe6ee706b6ae6a18fd30
piustech <barimue.emmanuel@gmail.com>
2022-12-21 05:22:38 -0800

    Task 1

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strncat - Write a function that concatenates two strings
+ * @dest: input string
+ * @src: input string
+ * @n: use bytes from src
+ * Return: a pointer to the resulting string dest
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i = 0, j = 0, destsrc = 0;
+
+	for (; dest[destsrc];)
+		destsrc++;
+
+	while (j < n && src[j])
+	{
+		dest[destsrc + i] = src[j];
+		i++;
+		j++;
+	}
+	return (dest);
+}
